eadb3243670e72e69ef68511afcb1df562435f94
Sid Swaminathan <sidswaminathan@yahoo.com>
2024-06-03 10:16:20 -0500

    Finalize Bike spec

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe Bike do
     before(:each) do
         @ride1 = Ride.new({
             name: "Walnut Creek Trail", 
-            distance: 10.7, loop: false, 
+            distance: 10.7, 
+            loop: false, 
             terrain: :hills
         })
 
@@ -27,29 +28,15 @@ RSpec.describe Bike do
 
     describe '#loop?' do
         it 'can indicate if the ride is a loop or not' do
-            expect(@ride.loop?()).to be(true)
+            expect(@ride1.loop?()).to be(false)
+            expect(@ride2.loop?()).to be(true)
         end
     end
 
     describe '#total_distance' do
         it 'can report its total distance' do
-            expect(@ride.total_distance()).to eq(21.4)
-        end
-    end
-
-    describe '#place' do
-        it 'can place a ship on the board' do
-            @board.place(@cruiser, ["A1", "A2", "A3"])
-            expect(@board.cells["A1"].ship).to eq(@cruiser)
-            expect(@board.cells["A2"].ship).to eq(@cruiser)
-            expect(@board.cells["A3"].ship).to eq(@cruiser)
-            expect(@board.cells["A4"].ship).to eq(nil)
-        end
-
-        it 'cant place a ship on the board if there is a ship there already' do
-            @board.place(@cruiser, ["A1", "A2", "A3"])
-            @board.place(@submarine, ["A1", "B1"])
-            expect(@board.cells["A1"].ship).to eq(@cruiser)
+            expect(@ride1.total_distance()).to eq(21.4)
+            expect(@ride2.total_distance()).to eq(14.9)
         end
     end
 end
